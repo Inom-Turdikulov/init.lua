fdc45da5b82a2d02bae935dc2c9f6dee7f3f3c7a
Inom T <i@inomoz.ru>
2023-07-26 01:51:50 +0600

    Add copilot config

---
+++ after/plugin/copilot.lua
@@ -0,0 +1,33 @@
+local ok, copilot = pcall(require, "copilot")
+if not ok then
+    return
+end
+
+-- I use copilot with copilot-cmp (cmp autocomplete plugin)
+-- so suggestion and panel are disabled
+copilot.setup({
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+        yaml = true,
+        markdown = true,
+        python = true,
+        lua = true,
+        c = true,
+        cpp = true,
+        rust = true,
+        json = true,
+        sh = function()
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+                -- disable for .env files
+                return false
+            end
+            return true
+        end,
+        ["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
+    },
+})
+
+-- Toggle Copilot
+vim.keymap.set("", "<M-\\>", ":Copilot toggle<CR>",
+{ noremap = true, silent = true })
