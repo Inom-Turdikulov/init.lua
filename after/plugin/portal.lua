f75fce4348a54c360ae1b7457c16ee94cd622da3
Inom T <i@inomoz.ru>
2023-07-25 18:08:27 +0600

    Add portal.nvim

---
+++ after/plugin/portal.lua
@@ -0,0 +1,6 @@
+if not pcall(require, "portal") then
+  return
+end
+
+vim.keymap.set("n", "<leader>o", "<cmd>Portal jumplist backward<cr>")
+vim.keymap.set("n", "<leader>i", "<cmd>Portal jumplist forward<cr>")
