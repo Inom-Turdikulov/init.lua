0611f8451f00c2db7dacc3a38f51c1b0288a8dcf
Inom T <i@inomoz.ru>
2023-07-25 18:28:37 +0600

    Add sniprun plugin

---
+++ after/plugin/sniprun.lua
@@ -0,0 +1,21 @@
+local ok, sniprun = pcall(require, "sniprun")
+if not ok then
+  return
+end
+
+sniprun.setup({
+    interpreter_options = {
+        C_original = {
+            compiler = "gcc"
+        },
+        Python3_original = {
+            interpreter = "python",
+        },
+    },
+})
+
+-- Select block by viB or vaB, and run SnipRun by f key
+-- TODO: add descriptions
+vim.api.nvim_set_keymap('v', 'f', '<Plug>SnipRun', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ze', '<Plug>SnipRunOperator', { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>zee', '<Plug>SnipRun', { silent = true })
