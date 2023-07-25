8d31d83be18e2ab85d9c8d17d0063cf48e49bb9c
Inom T <i@inomoz.ru>
2023-07-25 17:17:46 +0600

    Add folding configuration

---
+++ after/plugin/fold.lua
@@ -0,0 +1,45 @@
+vim.o.foldcolumn = '0' -- '0' is not bad
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
+vim.o.foldlevelstart = 99
+vim.o.foldenable = true
+
+-- Markdown conceallevel
+vim.cmd[[
+au FileType markdown setlocal foldlevel=99 conceallevel=2
+]]
+
+local ok, ufo = pcall(require, "ufo")
+if not ok then
+  return
+end
+
+if not pcall(require, "treesitter") then
+  print("TreeSitter not found, required for ufo!'")
+  return
+end
+
+-- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+vim.keymap.set('n', 'zR', ufo.openAllFolds)
+vim.keymap.set('n', 'zM', ufo.closeAllFolds)
+vim.keymap.set('n', 'zr', ufo.openFoldsExceptKinds)
+vim.keymap.set('n', 'zm', ufo.closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
+
+ufo.setup({
+    preview = {
+        win_config = {
+            border = { '', '─', '', '', '', '─', '', '' },
+            winhighlight = 'Normal:Folded',
+            winblend = 0
+        },
+        mappings = {
+            scrollU = '<C-u>',
+            scrollD = '<C-d>',
+            jumpTop = '[',
+            jumpBot = ']'
+        }
+    },
+    provider_selector = function(bufnr, filetype, buftype)
+        return { 'treesitter', 'indent' }
+    end
+})
