cea4f9bd3d99a2261ae430827c74578402aa6878
Inom Turdikulov <inom@iturdikulov.com>
2023-07-25 16:40:16 -0400

    Enable copilot on .nix

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -19,6 +19,7 @@ copilot.setup({
         cpp = true,
         rust = true,
         json = true,
+        nix = true,
         sh = function()
             if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
                 -- disable for .env files
