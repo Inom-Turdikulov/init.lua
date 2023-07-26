e62c463a54c5f275009b5d5ac47786d53fa4f29d
Inom T <i@inomoz.ru>
2023-07-26 10:16:23 +0600

    Improve dashit integration

--- after/plugin/dasht.lua
+++ after/plugin/dasht.lua
@@ -1,25 +1,36 @@
 -- Keymaps
 
--- search related docsets
-vim.keymap.set("n", "<leader>dk", ":Dasht<space>")
+-- Function to map keys, with [Telekasten] prefix for description
+local map = function(lhs, rhs, desc)
+    if desc then
+        desc = "[Dashit] " .. desc
+    end
+    vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
+end
 
--- search ALL the docsets
-vim.keymap.set("n", "<leader>dK", ":Dasht!<space>")
+map("<leader>dk", function()
+    local input = vim.fn.input("Dasht > ");
+    vim.cmd(":Dasht " .. input)
+end, "search related docsets")
 
--- search related docsets under cursor
-vim.keymap.set("n", "<leader>dw", ":call Dasht(dasht#cursor_search_terms())<Return>")
+map("<leader>dK", function()
+    local input = vim.fn.input("Dasht! > ");
+    vim.cmd(":Dasht! " .. input)
+end, "search ALL the docsets")
 
--- search ALL the docsets under cursor
-vim.keymap.set("n", "<leader>dW", ":call Dasht(dasht#cursor_search_terms(), '!'")
+map("<leader>dw", ":call Dasht(dasht#cursor_search_terms())<Return>", "search related docsets under cursor")
 
--- related docsets for your selected text
-vim.keymap.set("v", "<leader>ds", "y:<C-U>call Dasht(getreg(0))<Return>")
+map("<leader>dW", ":call Dasht(dasht#cursor_search_terms(), '!')<Return>", "search ALL the docsets under cursor")
 
--- related docsets for ALL the docsets
-vim.keymap.set("v", "<leader>dS", "y:<C-U>call Dasht(getreg(0), '!')<Return>")
+vim.keymap.set("v", "<leader>ds", "y:<C-U>call Dasht(getreg(0))<Return>", {
+    silent=true, desc="[Dashit] related docsets for your selected text"})
+
+vim.keymap.set("v", "<leader>dS", "y:<C-U>call Dasht(getreg(0), '!')<Return>",
+    {silent=true, desc="[Dashit] related docsets for ALL the docsets"})
 
 -- Specify related docsets for searching
 --  When in Python, also search NumPy, SciPy, and Pandas:
 vim.cmd[[
-let g:dasht_filetype_docsets['python'] = ['python', '(num|sci)py', 'pandas']
+let g:dasht_filetype_docsets = {} " filetype => list of docset name regexp
+let g:dasht_filetype_docsets['python'] = ['python', '(num|sci)py', 'pandas', 'SQLAlchemy', 'flask']
 ]]
