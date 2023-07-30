c4a310e4a06d4a910b66d42e1cc13577aab4b0ba
Inom Turdikulov <inom@iturdikulov.com>
2023-07-31 04:59:04 +0600

    Fix spell path

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -140,7 +140,7 @@ if ok then
             require("ltex_extra").setup {
                 load_langs = { "ru-RU", "en-US" },            -- table <string> : languages for witch dictionaries will be loaded
                 init_check = true,                            -- boolean : whether to load dictionaries on startup
-                path = vim.fn.stdpath("config") .. "spell",   -- string : path to store dictionaries. Relative path uses current working directory
+                path = vim.fn.stdpath("config") .. "/spell",   -- string : path to store dictionaries. Relative path uses current working directory
                 log_level = "none",                           -- string : "none", "trace", "debug", "info", "warn", "error", "fatal"
             }
         end,
