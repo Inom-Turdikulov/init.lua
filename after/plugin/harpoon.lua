28a64a63febfab6c9532cd233da9aa21921189dc
Inom T <i@inomoz.ru>
2023-08-03 18:20:57 +0600

    Adjust harpoon keybindings

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,36 +6,29 @@ end
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>a", mark.add_file,
-    { desc = "Add file to harpoon" })
+local map = function(lhs, rhs, desc)
+    if desc then desc = "[Harpoon] " .. desc end
 
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu,
-    { desc = "Toggle harpoon menu" })
+    vim.keymap.set("n", lhs, rhs, {silent = true, desc = desc})
+end
 
-vim.keymap.set("n", "<C-t>", function() ui.nav_file(1) end,
-    { desc = "Harpoon Navigate to file 1" })
-vim.keymap.set("n", "<C-n>", function() ui.nav_file(2) end,
-    { desc = "Harpoon Navigate to file 2" })
-vim.keymap.set("n", "<C-M-t>", function() ui.nav_file(3) end,
-    { desc = "Harpoon Navigate to file 3" })
-vim.keymap.set("n", "<C-M-n>", function() ui.nav_file(4) end,
-    { desc = "Harpoon Navigate to file 4" })
 
-vim.keymap.set("n", "<C-Bslash>",
-    function() require("harpoon.term").gotoTerminal(1) end,
-    { desc = "Harpoon Navigate to terminal 1" })
+map("<leader>a", mark.add_file, "add file")
+map("<C-e>", ui.toggle_quick_menu, "menu")
 
-vim.keymap.set("n", "<C-M-Bslash>",
-    function() require("harpoon.term").gotoTerminal(2) end,
-    { desc = "Harpoon Navigate to terminal 2" })
+map("<C-t>", function() ui.nav_file(1) end, "navigate to file 1")
+map("<C-n>", function() ui.nav_file(2) end, "navigate to file 2")
+map("<C-M-t>", function() ui.nav_file(3) end, "navigate to file 3")
+map("<C-M-n>", function() ui.nav_file(4) end, "navigate to file 4")
 
-vim.keymap.set("n", "<C-return>",
-    function() require("harpoon.term").gotoTerminal(3) end,
-    { desc = "Harpoon Navigate to terminal 3" })
-
-vim.keymap.set("n", "<C-M-return>",
-    function() require("harpoon.term").gotoTerminal(4) end,
-    { desc = "Harpoon Navigate to terminal 4" })
+map("<C-Bslash>", function() require("harpoon.term").gotoTerminal(1) end,
+    "navigate to terminal 1")
+map("<C-M-Bslash>", function() require("harpoon.term").gotoTerminal(2) end,
+    "navigate to terminal 2")
+map("<C-return>", function() require("harpoon.term").gotoTerminal(3) end,
+    "navigate to terminal 3")
+map("<C-M-return>", function() require("harpoon.term").gotoTerminal(4) end,
+    "navigate to terminal 4")
 
 harpoon.setup({
     -- Yes $HOME works
