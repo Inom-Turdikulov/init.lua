c4630971c6e8832387c49c933a1e3876e465b852
Inom T <i@inomoz.ru>
2023-08-08 03:22:01 +0600

    Add keybinding info, reformat

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,15 +1,13 @@
 local ok, copilot = pcall(require, "copilot")
-if not ok then
-    return
-end
+if not ok then return end
 
 -- I use copilot with copilot-cmp (cmp autocomplete plugin)
 -- so suggestion and panel are disabled
 -- you need to auth/validate copilot with github on first run
 -- `Copilot auth` or `Copilot status`
 copilot.setup({
-    suggestion = { enabled = false },
-    panel = { enabled = false },
+    suggestion = {enabled = false},
+    panel = {enabled = false},
     filetypes = {
         yaml = true,
         markdown = true,
@@ -21,16 +19,19 @@ copilot.setup({
         json = true,
         nix = true,
         sh = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), '^%.env.*') then
+            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)),
+                            '^%.env.*') then
                 -- disable for .env files
                 return false
             end
             return true
         end,
-        ["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
-    },
+        ["*"] = false -- disable for all other filetypes and ignore default `filetypes`
+    }
 })
 
 -- Toggle Copilot
 vim.keymap.set("", "<M-\\>", ":Copilot toggle<CR>",
-{ noremap = true, silent = true })
+               {silent = true, desc = "[Copilot] Toggle"})
+
+
