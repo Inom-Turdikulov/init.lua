54c712b32c6e08a57770053be03b2f1957301bc0
Inom T <i@inomoz.ru>
2023-08-10 20:59:52 +0600

    Update telescope mapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,49 +3,6 @@ if not ok then
   return
 end
 
-local builtin = require('telescope.builtin')
-
--- find telescope builtins
-vim.keymap.set("n", "<leader>fF", function()
-    builtin.builtin()
-end, { noremap = true, silent = true }, { desc = 'Telescope builtins' })
-
-vim.keymap.set('n', '<leader>fs', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-
--- Search - [ ] todo items in markdown files, autofill [ ] and press enter
-vim.keymap.set('n', '<leader>fS', function()
-    builtin.grep_string({ search = '- [ ]' });
-end)
-
-vim.keymap.set({ "n", "i", "v" }, '<M-p>', function()
-    if vim.fn.filereadable('.git/HEAD') == 1 then
-        builtin.git_files()
-    else
-        builtin.find_files()
-    end
-end, { desc = 'Telescope find files' })
-
-vim.keymap.set('n', '<M-P>', function()
-    builtin.find_files()
-end, { desc = 'Telescope find files' })
-
-vim.keymap.set("n", "<M-e>", function()
-    builtin.oldfiles({ only_cwd = true })
-end, { noremap = true, silent = true }, { desc = 'Telescope oldfiles' })
-
-vim.keymap.set('n', '<leader>fk', builtin.keymaps, { desc = 'Keymaps' })
-
-vim.keymap.set('n', '<leader>fK', builtin.commands, { desc = 'Commands' })
-
-vim.keymap.set('n', '<leader>fc', builtin.command_history,
-    { desc = 'Command history' })
-
-vim.keymap.set('n', '<M-b>', function()
-    builtin.buffers()
-end, { desc = 'Telescope buffers' })
-
 telescope.setup {
     defaults = {
         file_ignore_patterns = {
@@ -72,15 +29,55 @@ telescope.setup {
     },
 }
 
+local builtin = require('telescope.builtin')
+
+local map = function(lhs, rhs, desc)
+  if desc then
+    desc = "[Telescope] " .. desc
+  end
+
+  vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
+end
+
+-- find telescope builtins
+map("<leader>fF", builtin.builtin, "builtins")
+
+map('<leader>fs', function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") });
+end, "grep")
+
+-- Search - [ ] todo items in markdown files, autofill [ ] and press enter
+-- map('<leader>fS', function() builtin.grep_string({ search = '- [ ]') end, "todo items")
+
+vim.keymap.set({ "n", "i", "v" }, '<M-p>', function()
+    if vim.fn.filereadable('.git/HEAD') == 1 then
+        builtin.git_files()
+    else
+        builtin.find_files()
+    end
+end, { desc = '[Telescope] find files'})
+
+map('<M-P>', builtin.find_files, 'Telescope find files')
+
+map("<M-e>", function() builtin.oldfiles({ only_cwd = true }) end, 'oldfiles')
+
+map('<leader>fk', builtin.keymaps, 'keymaps')
+
+map('<leader>fK', builtin.commands, 'commands')
+
+map('<leader>fc', builtin.command_history, 'commands history')
+
+map('<M-b>', builtin.buffers, 'buffers')
+
 -- File browser
 if pcall(require, "telescope._extensions.file_browser") then
     telescope.load_extension("file_browser")
 end
 
 -- open file_browser with the path of the current buffer
-vim.keymap.set("n", "<leader>pV", function()
+map("<leader>pV", function()
     require("telescope").extensions.file_browser.file_browser({
         path = vim.fn.expand("%:p:h"),
         select_buffer = true,
     })
-end, {})
+end, "File Browser")
