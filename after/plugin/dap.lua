e4f0ee2ee5e749c100ba3fb7a87ad2dd659320e5
Inom Turdikulov <i@inomoz.ru>
2023-09-18 04:02:36 +0600

    Add JS debbuger support

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -187,9 +187,8 @@ end
 -- debugpy, pytest
 local has_dap_python, dap_python = pcall(require, "dap-python")
 if has_dap_python then
-    local python_path = 'python3'
-    if vim.env.PYTHONPATH then python_path = vim.env.PYTHONPATH end
-
+    local python_path = vim.fn.exepath('python')
+    if vim.env.LOCAL_PYTHONPATH then python_path = vim.env.LOCAL_PYTHONPATH end
     vim.g.python3_host_prog = python_path
     dap_python.setup(vim.g.python3_host_prog)
     dap_python.test_runner = 'pytest'
@@ -278,6 +277,64 @@ if lldb_path then
     -- If you want to use this for Rust and C, add something like this:
     dap.configurations.c = dap.configurations.cpp
     dap.configurations.rust = dap.configurations.cpp
+end
+
+require("dap-vscode-js").setup({
+    -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
+    -- TODO: parametrize this path
+    -- debugger_path = vim.fn.expand(""), -- Path to vscode-js-debug installation.
+    debugger_cmd = { "dap-node" }, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    adapters = { 'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal', 'pwa-extensionHost', 'node', 'chrome' }, -- which adapters to register in nvim-dap
+    -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
+    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
+    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+})
+
+local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+for _, language in ipairs(js_based_languages) do
+  require("dap").configurations[language] = {
+    {
+      type = "pwa-node",
+      request = "launch",
+      name = "Launch file",
+      program = "${file}",
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-node",
+      request = "attach",
+      name = "Attach",
+      processId = require 'dap.utils'.pick_process,
+      cwd = "${workspaceFolder}",
+    },
+    {
+      type = "pwa-chrome",
+      request = "launch",
+      name = "Start Chrome with \"localhost\"",
+      url = "http://localhost:3000",
+      webRoot = "${workspaceFolder}",
+      userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+      runtimeExecutable = "/etc/profiles/per-user/inom/bin/brave"
+    },
+        {
+            -- use nvim-dap-vscode-js's pwa-chrome debug adapter
+            type = "pwa-chrome",
+            request = "launch",
+            -- name of the debug action
+            name = "Launch Chrome to debug client side code",
+            -- default vite dev server url
+            url = "http://localhost:3000",
+            -- for TypeScript/Svelte
+            sourceMaps = true,
+            webRoot = "${workspaceFolder}/src",
+            protocol = "inspector",
+            port = 9222,
+            -- skip files from vite's hmr
+            skipFiles = { "**/node_modules/**/*", "**/@vite/*", "**/src/client/*", "**/src/*" },
+            runtimeExecutable = "/etc/profiles/per-user/inom/bin/brave"
+        }
+    }
 end
 
 -- -- C configuration
