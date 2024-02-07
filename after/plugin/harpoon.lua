75863abf6e80a16160d5f109c8d2fd3adeca177b
Inom Turdikulov <i@inomoz.ru>
2024-02-07 19:45:10 +0600

    Drop telescope support

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -3,22 +3,6 @@ if not ok then return end
 
 harpoon:setup({})
 
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({results = file_paths}),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({})
-    }):find()
-end
-
 local map = function(lhs, rhs, desc)
     if desc then desc = "[Harpoon] " .. desc end
 
@@ -26,8 +10,7 @@ local map = function(lhs, rhs, desc)
 end
 
 map("<leader>a", function() harpoon:list():append() end, "Add File")
-map("<C-e>", function() toggle_telescope(harpoon:list()) end,
-    "Open harpoon window")
+map("<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Open harpoon UI")
 map("<C-t>", function() harpoon:list():select(1) end, "Navigate to file 1")
 map("<C-n>", function() harpoon:list():select(2) end, "Navigate to file 2")
 map("<C-M-t>", function() harpoon:list():select(3) end, "Navigate to file 3")
