609b12cbd98fda540bccac4a97f86c54c143e493
Inom Turdikulov <i@inomoz.ru>
2024-02-24 19:16:45 +0600

    Adjust formatting ignore leader key

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -1,10 +1,15 @@
-local ok, whichKey = pcall(require, "which-key")
+local ok, which_key = pcall(require, "which-key")
 if not ok then return end
 
-whichKey.setup {
+which_key.setup {
     icons = {
         breadcrumb = ">>", -- symbol used in the command line area that shows your active key combo
         separator = ">", -- symbol used between a key and it's label
         group = "+" -- symbol prepended to a group
-    }
+    },
+    triggers_blacklist = {
+        -- list of mode / prefixes that should never be hooked by WhichKey
+        -- this is mostly relevant for keymaps that start with a native binding
+        n = { "<leader>" },
+    },
 }
