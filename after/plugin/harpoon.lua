32530471ea4c2bef19d3abffac4fba888c1f2e84
Inom Turdikulov <i@inomoz.ru>
2024-03-01 22:42:59 +0500

    Adjust keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,21 +1,41 @@
 local ok, harpoon = pcall(require, "harpoon")
-if not ok then return end
-
-harpoon:setup({})
-
-local map = function(lhs, rhs, desc)
-    if desc then desc = "[Harpoon] " .. desc end
-
-    vim.keymap.set("n", lhs, rhs, {silent = true, desc = desc})
+if not ok then
+    return
 end
 
-map("<leader>a", function() harpoon:list():append() end, "Add File")
-map("<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, "Open harpoon UI")
-map("<C-t>", function() harpoon:list():select(1) end, "Navigate to file 1")
-map("<C-n>", function() harpoon:list():select(2) end, "Navigate to file 2")
-map("<C-M-t>", function() harpoon:list():select(3) end, "Navigate to file 3")
-map("<C-M-n>", function() harpoon:list():select(4) end, "Navigate to file 4")
+harpoon:setup({
+  settings = {
+    save_on_toggle = true,
+  },
+})
+
+local map = function(lhs, rhs, desc)
+    if desc then
+        desc = "[Harpoon] " .. desc
+    end
+
+    vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
+end
+
+map("<leader>a", function()
+    harpoon:list():append()
+end, "Add File")
+
+map("<C-e>", function()
+    harpoon.ui:toggle_quick_menu(harpoon:list())
+end, "Open harpoon UI")
+
+local bindings = { 't', 'm', 'g', 'n'  }
+for k,v in pairs(bindings) do
+  map("<C-" .. v .. ">", function() harpoon:list():select(k) end, "Navigate to file " .. k)
+end
 
 -- Toggle previous & next buffers stored within Harpoon list
-map("<C-M-p>", function() harpoon:list():prev() end, "Toggle Next buffer")
-map("<C-M-n>", function() harpoon:list():next() end, "Toggle Previous buffer")
+map("<C-M-P>", function()
+    harpoon:list():next()
+end, "Toggle Previous buffer")
+
+map("<C-M-N>", function()
+    harpoon:list():prev()
+end, "Toggle Next buffer")
+
