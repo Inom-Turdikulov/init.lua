ac9b61529a18b641cd7ffcc2af1a5772fbc44338
Inom Turdikulov <i@inomoz.ru>
2024-03-02 17:15:05 +0500

    Disable triggers blacklist

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -6,10 +6,5 @@ which_key.setup {
         breadcrumb = ">>", -- symbol used in the command line area that shows your active key combo
         separator = ">", -- symbol used between a key and it's label
         group = "+" -- symbol prepended to a group
-    },
-    triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for keymaps that start with a native binding
-        n = { "<leader>" },
-    },
+    }
 }
