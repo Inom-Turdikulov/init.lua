99311da14d7e78555d8c77e96af9084a1ca30a79
Inom Turdikulov <i@inomoz.ru>
2024-03-02 17:14:10 +0500

    Drop portal plugin

--- after/plugin/portal.lua
+++
@@ -1,4 +0,0 @@
-if not pcall(require, "portal") then return end
-
-vim.keymap.set("n", "<leader>o", "<cmd>Portal jumplist backward<cr>")
-vim.keymap.set("n", "<leader>i", "<cmd>Portal jumplist forward<cr>")
