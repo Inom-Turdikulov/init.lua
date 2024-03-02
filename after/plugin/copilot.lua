60aefd42cb2f1ff9cb153050176e2b94417e2c29
Inom Turdikulov <i@inomoz.ru>
2024-03-02 16:32:59 +0500

    Adjust copilot integration

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,43 +1,68 @@
-local ok, copilot = pcall(require, "copilot")
-if not ok then return end
-
--- I use copilot with copilot-cmp (cmp autocomplete plugin)
--- so suggestion and panel are disabled
--- you need to auth/validate copilot with github on first run
--- `Copilot auth` or `Copilot status`
-copilot.setup({
-    suggestion = {enabled = false},
-    panel = {enabled = false},
-    filetypes = {
-        gitcommit = true,
-        yaml = true,
-        markdown = true,
-        python = true,
-        javascript = true,
-        html = true,
-        jinja = true,
-        css = true,
-        scss = true,
-        lua = true,
-        c = true,
-        cpp = true,
-        rust = true,
-        json = true,
-        nix = true,
-        bash = true,
-        zsh = true,
-        sh = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)),
-                            '^%.env.*') then
-                -- disable for .env files
-                return false
-            end
-            return true
-        end,
-        ["*"] = false -- disable for all other filetypes and ignore default `filetypes`
-    }
-})
-
--- Toggle Copilot
-vim.keymap.set("", "<M-\\>", ":Copilot toggle<CR>",
-               {silent = true, desc = "[Copilot] Toggle"})
+-- local ok, copilot = pcall(require, "copilot")
+-- if not ok then return end
+--
+-- -- I use copilot with copilot-cmp (cmp autocomplete plugin)
+-- -- so suggestion and panel are disabled
+-- -- you need to auth/validate copilot with github on first run
+-- -- `Copilot auth` or `Copilot status`
+-- copilot.setup({
+--     suggestion = {
+--         enable = false,
+--         keymap = {
+--             accept = "<C-y>",
+--             next = "<M-n>",
+--             prev = "<M-t>",
+--             dismiss = "<C-]>",
+--         },
+--     },
+--     filetypes = {
+--         gitcommit = true,
+--         yaml = true,
+--         markdown = true,
+--         python = true,
+--         javascript = true,
+--         html = true,
+--         jinja = true,
+--         css = true,
+--         scss = true,
+--         lua = true,
+--         c = true,
+--         cpp = true,
+--         rust = true,
+--         json = true,
+--         nix = true,
+--         bash = true,
+--         zsh = true,
+--         sh = function()
+--             if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)),
+--                     '^%.env.*') then
+--                 -- disable for .env files
+--                 return false
+--             end
+--             return true
+--         end,
+--         ["*"] = false -- disable for all other filetypes and ignore default `filetypes`
+--     }
+-- })
+--
+--
+-- -- Custom keymap
+-- local cmp = require("cmp")
+-- local copilot_suggestion = require("copilot.suggestion")
+--
+-- vim.keymap.set('i', '<M-n>', function()
+--     copilot_suggestion.next()
+--     if cmp.visible() then cmp.abort() end
+-- end)
+--
+-- vim.keymap.set('i', '<M-p>', function()
+--     copilot_suggestion.prev()
+--     if cmp.visible() then cmp.abort() end
+-- end)
+--
+-- vim.keymap.set('i', '<M-y>', function()
+--     copilot_suggestion.accept()
+-- end)
+-- vim.keymap.set('i', '<M-S-y>', function()
+--     if copilot_suggestion.is_visible() then copilot_suggestion.dismiss() end
+-- end, {noremap=true, silent=true})
