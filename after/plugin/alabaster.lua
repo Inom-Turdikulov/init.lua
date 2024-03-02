fa610de6b657078f4aa680b800c398800058f6e2
Inom Turdikulov <i@inomoz.ru>
2024-03-02 16:23:31 +0500

    Adjust specific hlgroups

--- after/plugin/alabaster.lua
+++ after/plugin/alabaster.lua
@@ -3,3 +3,9 @@ vim.cmd("colorscheme alabaster")
 -- Customize theme
 vim.api.nvim_set_hl(0,'ColorColumn',{ bg='#30363f'})
 vim.api.nvim_set_hl(0,'NonText',{ fg='#30363f'})
+vim.api.nvim_set_hl(0,'CopilotAnnotation',{ fg='#9d9c9c'})
+vim.api.nvim_set_hl(0,'CopilotSuggestion',{ fg='#9d9c9c'})
+
+-- Express line
+vim.api.nvim_set_hl(0,'ElInsert',{ fg='white', bg='green'})
+
