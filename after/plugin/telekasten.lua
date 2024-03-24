42256ca12c1db2586969ca521a15efe2c6eadd12
Inom Turdikulov <i@inomoz.ru>
2024-03-24 13:20:59 +0600

    Adjust styling, keybindings and formatting

--- after/plugin/telekasten.lua
+++ after/plugin/telekasten.lua
@@ -9,7 +9,7 @@ end
 -- Function to map keys, with [Telekasten] prefix for description
 local map = function(lhs, rhs, desc)
     if desc then desc = "[Telekasten] " .. desc end
-    vim.keymap.set("n", lhs, rhs, {silent = true, desc = desc})
+    vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
 end
 
 local wiki_home = vim.fn.expand("~/Wiki")
@@ -24,10 +24,21 @@ telekasten.setup({
     plug_into_calendar = true,
     sort = "modified",
     auto_set_filetype = false,
-    -- follow_url_fallback = "call jobstart('linkhandler {{url}}')",
-    -- dir names for special notes (absolute path or subdir name)
     new_note_filename = "title",
-    media_previewer = "telescope-media-files"
+    media_previewer = "telescope-media-files",
+    enable_create_new = false, -- I use C-n to navigate files
+    media_extensions = {
+        ".png",
+        ".jpg",
+        ".bmp",
+        ".gif",
+        ".webm",
+        ".webp",
+        ".svg",
+        ".pdf",
+        ".mp4",
+        ".ico"
+    },
 })
 
 map("<leader>tf", function() telekasten.find_notes() end, "find notes")
@@ -42,30 +53,30 @@ map("<leader>tI", telekasten.insert_img_link, "insert image link")
 
 map("<leader>tL", telekasten.paste_img_and_link, "paste image and link")
 
-vim.keymap.set("n", "<a-cr>", function()
-    if vim.bo.filetype == "markdown" then telekasten.follow_link() end
-end, {silent = true, desc = "[Telekasten] follow link"})
+map("<leader>tl", function()
+    telekasten.follow_link()
+end, "follow link")
 
 map("<leader>tn", telekasten.new_note, "create new note")
 
 map("<leader>tN", telekasten.new_templated_note, "create new note from template")
 
-map("<leader>tY", telekasten.yank_notelink, "yank notelink")
+map("<leader>ty", telekasten.yank_notelink, "yank notelink")
 
 map("<leader>tc", telekasten.show_calendar, "show calendar")
 
 vim.keymap.set("n", "<leader>tz",
-               function() telekasten.toggle_todo({onlyTodo = true}) end,
-               {silent = true, desc = "[Telekasten] toggle TODO (only TODO)"})
+    function() telekasten.toggle_todo({ onlyTodo = true }) end,
+    { silent = true, desc = "[Telekasten] toggle TODO (only TODO)" })
 
 vim.keymap.set("v", "<leader>tz", function()
-    telekasten.toggle_todo({onlyTodo = true, v = true})
-end, {silent = true, desc = "[Telekasten] toggle TODO (only TODO)"})
+    telekasten.toggle_todo({ onlyTodo = true, v = true })
+end, { silent = true, desc = "[Telekasten] toggle TODO (only TODO)" })
 
-map("<leader>tT", telekasten.toggle_todo, "toggle TODO")
+map("<leader>tt", telekasten.toggle_todo, "toggle TODO")
 vim.keymap.set("v", "<leader>tT",
-               function() telekasten.toggle_todo({v = true}) end,
-               {silent = true, desc = "[Telekasten] toggle TODO"})
+    function() telekasten.toggle_todo({ v = true }) end,
+    { silent = true, desc = "[Telekasten] toggle TODO" })
 
 map("<leader>tb", telekasten.show_backlinks, "show backlinks")
 
@@ -83,6 +94,6 @@ map("<leader>to", function()
     local current_line = vim.fn.line(".")
     local obsidian_url =
         "obsidian://advanced-uri?vault=Wiki&viewmode=preview&filepath=" ..
-            bufname .. "&line=" .. current_line
-    vim.fn.jobstart({'xdg-open', obsidian_url})
+        bufname .. "&line=" .. current_line
+    vim.fn.jobstart({ 'xdg-open', obsidian_url })
 end, "open in obsidian")
