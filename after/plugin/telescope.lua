d40ff1b43558435fe4f98d37b179b6bd6f7acf68
Inom Turdikulov <i@inomoz.ru>
2024-03-24 13:20:17 +0600

    Global telescope plugin update
    Use square border, adjust keybindings and plugins

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,10 +1,31 @@
 local ok, telescope = pcall(require, "telescope")
-if not ok then return end
+if not ok then
+    return
+end
 
-telescope.setup {
-    defaults = {file_ignore_patterns = {"./node_modules"}},
+local builtin = require("telescope.builtin")
+
+local map = function(lhs, rhs, desc)
+    if desc then
+        desc = "[Telescope] " .. desc
+    end
+
+    vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
+end
+
+telescope.setup({
+    defaults = {
+        file_ignore_patterns = { "./node_modules" },
+        borderchars = { "─", "│", "─", "│", "┌", "┐", "┘", "└" },
+    },
     extensions = {
-        media_files = {filetypes = {"png", "webp", "jpg", "jpeg", "svg", "pdf"}},
+        ["ui-select"] = {
+            require("telescope.themes").get_dropdown(),
+        },
+        media_files = {
+            filetypes = { "png", "jpg", "gif", "mp4", "webm", "webp", "pdf" },
+            find_cmd = "find",
+        },
         file_browser = {
             theme = "ivy",
             -- disables netrw and use telescope-file-browser in its place
@@ -15,41 +36,39 @@ telescope.setup {
                 },
                 ["n"] = {
                     -- your custom normal mode mappings
-                }
-            }
-        }
-    }
-}
+                },
+            },
+        },
+    },
+})
 
-local builtin = require("telescope.builtin")
-
-local map = function(lhs, rhs, desc)
-    if desc then desc = "[Telescope] " .. desc end
-
-    vim.keymap.set("n", lhs, rhs, {silent = true, desc = desc})
-end
+-- Enable telescope extensions, if they are installed
+pcall(telescope.load_extension, "fzf")
+pcall(telescope.load_extension, "ui-select")
+pcall(telescope.load_extension, "media_files")
 
 -- find telescope builtins
-map("<leader>fF", builtin.builtin, "builtins")
+map("<leader>fb", builtin.builtin, "builtins")
 
-map("<leader>fs",
-    function() builtin.grep_string({search = vim.fn.input("Grep > ")}); end,
-    "grep")
+map('<leader>ff', builtin.find_files, "[F]ind [F]iles")
 
--- Search - [ ] todo items in markdown files, autofill [ ] and press enter
--- map("<leader>fS", function() builtin.grep_string({ search = "- [ ]") end, "todo items")
-
-vim.keymap.set({"n", "i", "v"}, "<M-p>", function()
+map('<PageDown>', function ()
     if vim.fn.filereadable(".git/HEAD") == 1 then
         builtin.git_files()
     else
-        builtin.find_files()
+        vim.notify("Not a git repository", "error")
     end
-end, {desc = "[Telescope] find files"})
+end, "Git files")
 
-map("<M-P>", builtin.find_files, "Telescope find files")
+map("<leader>fs", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end, "grep")
 
-map("<M-e>", function() builtin.oldfiles({only_cwd = true}) end, "oldfiles")
+map("<M-e>", function()
+    builtin.oldfiles({ only_cwd = true })
+end, "oldfiles")
+
+map('<leader>fh', builtin.help_tags, "Help tags")
 
 map("<leader>fk", builtin.keymaps, "keymaps")
 
@@ -57,22 +76,9 @@ map("<leader>fK", builtin.commands, "commands")
 
 map("<leader>fc", builtin.command_history, "commands history")
 
-map("<M-b>", builtin.buffers, "buffers")
-
--- File browser
-if pcall(require, "telescope._extensions.file_browser") then
-    telescope.load_extension("file_browser")
-end
-
--- Previewer
-if pcall(require, "telescope._extensions.media_files") then
-    telescope.load_extension("media_files")
-end
-
--- open file_browser with the path of the current buffer
-map("<leader>pV", function()
-    require("telescope").extensions.file_browser.file_browser({
-        path = vim.fn.expand("%:p:h"),
-        select_buffer = true
+-- Shortcut for searching your neovim configuration files
+map("<leader>vpP", function()
+    builtin.find_files({
+        cwd = vim.fn.stdpath("config"),
     })
-end, "File Browser")
+end, "neovim files")
