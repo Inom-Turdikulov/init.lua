cf440c6f724e4aea1ffb897ce76d84f15113876d
Inom Turdikulov <i@inomoz.ru>
2024-03-24 13:17:44 +0600

    Global treesitter config update
    New mappings, disable context plugin for Markdown, and more

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,82 +1,95 @@
 if not pcall(require, "nvim-treesitter") then return end
 
-require'nvim-treesitter.configs'.setup {
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = false,
+-- [[ Configure Treesitter ]]
+-- See `:help nvim-treesitter`
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+    require('nvim-treesitter.configs').setup {
+        -- Add languages to be installed here that you want installed for treesitter
+        ensure_installed = {
+            "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript",
+            "tsx", "html", "css", "markdown", "markdown_inline", "scss", "bash", "rust",
+            "go", "gdscript", "python", "json", "toml", "tsv", "csv", "yaml",
+            "nix", "sql"
+        },
 
-    ensure_installed = {
-        "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript",
-        "html", "css", "markdown", "markdown_inline", "scss", "bash", "rust",
-        "go", "gdscript", "cpp", "python", "json", "toml", "tsv", "csv", "yaml",
-        "nix"
-    },
-
-    highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-        -- Disable slow treesitter highlight for large files
-        disable = function(lang, buf)
-            local max_filesize = 500 * 1024 -- 500 KB
-            local ok, stats = pcall(vim.loop.fs_stat,
-                                    vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
-        end
-    },
-    -- Indentation based on treesitter for the = operator. NOTE: This is an experimental feature.
-    indent = {enable = true},
-    textobjects = {
-        select = {
+        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        auto_install = false,
+        -- Install languages synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+        -- List of parsers to ignore installing
+        ignore_install = {},
+        modules = {},
+        highlight = {
             enable = true,
-            -- Automatically jump forward to textobj, similar to targets.vim
-            lookahead = true,
+
+            -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+            -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+            -- Using this option may slow down your editor, and you may see some duplicate highlights.
+            -- Instead of true it can also be a list of languages
+            additional_vim_regex_highlighting = false,
+        },
+        indent = { enable = true },
+        incremental_selection = {
+            enable = true,
             keymaps = {
-                -- You can use the capture groups defined in textobjects.scm
-                ["aB"] = "@block.outer",
-                ["iB"] = "@block.inner",
-                ["ac"] = "@class.outer",
-                -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                -- nvim_buf_set_keymap) which plugins like which-key display
-                ["ic"] = {
-                    query = "@class.inner",
-                    desc = "Select inner part of a class region"
+                init_selection = '<m-space>',
+                node_incremental = '<m-space>',
+                scope_incremental = '<m-s>',
+                node_decremental = '<M-q>',
+            },
+        },
+        textobjects = {
+            select = {
+                enable = true,
+                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                keymaps = {
+                    -- You can use the capture groups defined in textobjects.scm
+                    ['aa'] = '@parameter.outer',
+                    ['ia'] = '@parameter.inner',
+                    ['af'] = '@function.outer',
+                    ['if'] = '@function.inner',
+                    ['ac'] = '@class.outer',
+                    ['ic'] = '@class.inner',
                 },
-                -- You can also use captures from other query groups like `locals.scm`
-                ["as"] = {
-                    query = "@scope",
-                    query_group = "locals",
-                    desc = "Select language scope"
-                }
             },
-            -- You can choose the select mode (default is charwise 'v')
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * method: eg 'v' or 'o'
-            -- and should return the mode ('v', 'V', or '<c-v>') or a table
-            -- mapping query_strings to modes.
-            selection_modes = {
-                ['@parameter.outer'] = 'v', -- charwise
-                ['@function.outer'] = 'V', -- linewise
-                ['@class.outer'] = '<c-v>' -- blockwise
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    [']m'] = '@function.outer',
+                    [']]'] = '@class.outer',
+                },
+                goto_next_end = {
+                    [']M'] = '@function.outer',
+                    [']['] = '@class.outer',
+                },
+                goto_previous_start = {
+                    ['[m'] = '@function.outer',
+                    ['[['] = '@class.outer',
+                },
+                goto_previous_end = {
+                    ['[M'] = '@function.outer',
+                    ['[]'] = '@class.outer',
+                },
             },
-            -- If you set this to `true` (default is `false`) then any textobject is
-            -- extended to include preceding or succeeding whitespace. Succeeding
-            -- whitespace has priority in order to act similarly to eg the built-in
-            -- `ap`.
-            --
-            -- Can also be a function which gets passed a table with the keys
-            -- * query_string: eg '@function.inner'
-            -- * selection_mode: eg 'v'
-            -- and should return true of false
-            include_surrounding_whitespace = true
-        }
+            -- swap = {
+            --     enable = true,
+            --     swap_next = {
+            --         ['<leader>a'] = '@parameter.inner',
+            --     },
+            --     swap_previous = {
+            --         ['<leader>A'] = '@parameter.inner',
+            --     },
+            -- },
+        },
     }
-}
+    require 'treesitter-context'.setup {
+        on_attach = function (buf)
+            if vim.api.nvim_buf_get_option(buf, 'filetype') == 'markdown' then
+                return false
+            end
+            return true
+        end
+    }
+end, 0)
