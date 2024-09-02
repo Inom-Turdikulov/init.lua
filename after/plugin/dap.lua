874786c9a7c5d46db685b9f348aab84a2956b160
Inom Turdikulov <i@inomoz.ru>
2024-09-02 17:26:24 +0300

    Simplify/fix dap debug config for javascript

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -250,21 +250,31 @@ if lldb_path then
     dap.configurations.rust = dap.configurations.cpp
 end
 
+
+-- JS debugging configuration
+
 require("dap-vscode-js").setup({
-    -- node_path = "node", -- Path of node executable. Defaults to $NODE_PATH, and then "node"
-    -- TODO: parametrize this path
-    -- debugger_path = vim.fn.expand(""), -- Path to vscode-js-debug installation.
-    debugger_cmd = {"dap-node"}, -- Command to use to launch the debug server. Takes precedence over `node_path` and `debugger_path`.
+    debugger_path = vim.fn.exepath("js-debug"),
     adapters = {
         'chrome', 'pwa-node', 'pwa-chrome', 'pwa-msedge', 'node-terminal',
         'pwa-extensionHost', 'node', 'chrome'
-    } -- which adapters to register in nvim-dap
-    -- log_file_path = "(stdpath cache)/dap_vscode_js.log" -- Path for file logging
-    -- log_file_level = false -- Logging level for output to file. Set to false to disable file logging.
-    -- log_console_level = vim.log.levels.ERROR -- Logging level for output to console. Set to false to disable console output.
+    }
 })
 
-local js_based_languages = {"typescript", "javascript", "typescriptreact"}
+local js_based_languages = { "typescript", "javascript", "typescriptreact" }
+
+local adaptre_settings = {
+    type = 'server',
+    host = 'localhost',
+    port = '${port}',
+    executable = {
+        command = 'js-debug',
+        args = { '${port}' },
+    }
+}
+
+dap.adapters['pwa-node'] = adaptre_settings
+dap.adapters['pwa-chrome'] = adaptre_settings
 
 for _, language in ipairs(js_based_languages) do
     require("dap").configurations[language] = {
@@ -273,41 +283,70 @@ for _, language in ipairs(js_based_languages) do
             request = "launch",
             name = "Launch file",
             program = "${file}",
-            cwd = "${workspaceFolder}"
+            cwd = "${workspaceFolder}",
         }, {
-            type = "pwa-node",
-            request = "attach",
-            name = "Attach",
-            processId = require'dap.utils'.pick_process,
-            cwd = "${workspaceFolder}"
-        }, {
-            type = "pwa-chrome",
-            request = "launch",
-            name = "Start Chrome with \"localhost\"",
-            url = "http://localhost:3000",
-            webRoot = "${workspaceFolder}",
-            userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
-            runtimeExecutable = "/etc/profiles/per-user/inom/bin/brave"
-        }, {
-            -- use nvim-dap-vscode-js's pwa-chrome debug adapter
-            type = "pwa-chrome",
-            request = "launch",
-            -- name of the debug action
-            name = "Launch Chrome to debug client side code",
-            -- default vite dev server url
-            url = "http://localhost:3000",
-            -- for TypeScript/Svelte
-            sourceMaps = true,
-            webRoot = "${workspaceFolder}/src",
-            protocol = "inspector",
-            port = 9222,
-            -- skip files from vite's hmr
-            skipFiles = {
-                "**/node_modules/**/*", "**/@vite/*", "**/src/client/*",
-                "**/src/*"
-            },
-            runtimeExecutable = "/etc/profiles/per-user/inom/bin/brave"
-        }
+        type = "pwa-node",
+        request = "attach",
+        name = "Attach to process",
+        processId = require 'dap.utils'.pick_process,
+        cwd = "${workspaceFolder}"
+    }, {
+        type = "pwa-chrome",
+        request = "launch",
+        name = "Start Chrome with \"localhost\"",
+        url = "http://localhost:3000",
+        webRoot = "${workspaceFolder}",
+        userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+        runtimeExecutable = vim.fn.exepath("google-chrome"),
+        runtimeArgs = { "--remote-debugging-port=9222" }
+    }, {
+        type = "pwa-chrome",
+        request = "attach",
+        name = "Attach Chrome to debug client side code",
+        url = "http://localhost:3000",
+        webRoot = "${workspaceFolder}",
+        userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
+        port = 9222,
+    }, {
+        -- use nvim-dap-vscode-js's pwa-chrome debug adapter
+        type = "pwa-chrome",
+        request = "launch",
+        -- name of the debug action
+        name = "Launch Chrome to debug client side code",
+        -- default vite dev server url
+        url = "http://localhost:3000",
+        -- for TypeScript/Svelte
+        sourceMaps = true,
+        webRoot = "${workspaceFolder}/src",
+        protocol = "inspector",
+        port = 9222,
+        -- skip files from vite's hmr
+        skipFiles = {
+            "**/node_modules/**/*", "**/@vite/*", "**/src/client/*",
+            "**/src/*"
+        },
+        runtimeExecutable = vim.fn.exepath("brave"),
+        runtimeArgs = { "--remote-debugging-port=9222" }
+    }, {
+        name = 'Next.js: debug server-side',
+        type = 'pwa-node',
+        request = 'attach',
+        port = 9231,
+        skipFiles = { '<node_internals>/**', 'node_modules/**' },
+        cwd = '${workspaceFolder}',
+    }, {
+        name = 'Next.js: debug client-side',
+        type = 'pwa-chrome',
+        request = 'launch',
+        url = 'http://localhost:3000',
+        webRoot = '${workspaceFolder}',
+        sourceMaps = true,     -- https://github.com/vercel/next.js/issues/56702#issuecomment-1913443304
+        sourceMapPathOverrides = {
+            ['webpack://_N_E/*'] = '${webRoot}/*',
+        },
+        runtimeExecutable = vim.fn.exepath("google-chrome"),
+        runtimeArgs = { "--remote-debugging-port=9222" }
+    }
     }
 end
 
