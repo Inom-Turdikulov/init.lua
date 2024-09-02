88d8ec9918d707b8b7e5cc6163014065c8f10a63
Inom Turdikulov <i@inomoz.ru>
2024-09-02 16:47:42 +0300

    Adjust formatting

--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -4,7 +4,7 @@ if not ok then return end
 which_key.setup {
     icons = {
         breadcrumb = ">>", -- symbol used in the command line area that shows your active key combo
-        separator = ">", -- symbol used between a key and it's label
-        group = "+" -- symbol prepended to a group
+        separator = ">",   -- symbol used between a key and it's label
+        group = "+"        -- symbol prepended to a group
     }
 }
