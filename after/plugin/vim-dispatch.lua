9c0b790fe116ba1f9a3a4e0fd0f025ead7d04604
Inom Turdikulov <i@inomoz.ru>
2024-10-28 23:20:57 +0300

    Vim dispatch - integrate per file types

--- after/plugin/vim-dispatch.lua
+++ after/plugin/vim-dispatch.lua
@@ -2,9 +2,9 @@ if not vim.g.loaded_dispatch then return end
 
 local generalSettingsGroup = vim.api.nvim_create_augroup('Vim Dispatch', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { '*.lua' },
+    pattern = { '*.py' },
     callback = function()
-        vim.b.dispatch = 'lua %'
+        vim.b.dispatch = 'python %'
     end,
     group = generalSettingsGroup,
 })
