d4a4c0c2cd89e6d2e658dfff27f8713b3eb78ed4
Inom Turdikulov <i@inomoz.ru>
2024-10-28 23:06:56 +0300

    Supermaven - drop unused configuration

--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -1,6 +1,10 @@
 local ok, supermaven = pcall(require, "supermaven-nvim")
 if not ok then return end
 supermaven.setup({
+    color = {
+        suggestion_color = "#696969",
+        cterm = 244,
+    },
     log_level = "off",
     disable_inline_completion = true,
     disable_keymaps = true
