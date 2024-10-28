e1d548243ebca63e7b614e0c38059440cf97df4c
Inom Turdikulov <i@inomoz.ru>
2024-10-28 18:59:38 +0300

    Don't include module two times

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,6 +1,6 @@
 local ok, comment = pcall(require, "Comment")
 if not ok then return end
-require('Comment').setup({
+comment.setup({
     -- Fix comments for react components
     pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
 })
