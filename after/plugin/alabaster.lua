74cf690ed0f4c9409fd6c4224e53046987a6dd00
Inom Turdikulov <i@inomoz.ru>
2024-10-28 18:53:30 +0300

    Adjust theme colors

--- after/plugin/alabaster.lua
+++ after/plugin/alabaster.lua
@@ -2,7 +2,6 @@ vim.cmd("colorscheme alabaster")
 
 -- Customize theme
 vim.api.nvim_set_hl(0,'ColorColumn',{ bg='#30363f'})
-vim.api.nvim_set_hl(0,'NonText',{ fg='#444a52'})
 vim.api.nvim_set_hl(0,'CopilotAnnotation',{ fg='#9d9c9c'})
 vim.api.nvim_set_hl(0,'CopilotSuggestion',{ fg='#9d9c9c'})
 -- Visual selection
@@ -11,3 +10,6 @@ vim.api.nvim_set_hl(0,'Visual',{ bg = '#333444'})
 -- Express line
 vim.api.nvim_set_hl(0,'ElInsert',{ fg='white', bg='green'})
 
+-- Transparent background
+vim.api.nvim_set_hl(0, 'Normal', { bg='NONE'})
+vim.api.nvim_set_hl(0, 'NonText', { bg='NONE', fg='#444a52'})
